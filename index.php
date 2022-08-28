1d2c529a705e4200568ca816852b28105288e4dc
Hamilton-hbtechcompany <htadeu@hotmail.com>
2022-08-27 22:42:23 -0300

    Conclusão do desafio TOSHIRO SHIBAKITA

--- index.php
+++ index.php
@@ -1,48 +1,64 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
-<title>Exemplo PHP</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exemplo PHP</title>
 </head>
 <body>
-
-<?php
-ini_set("display_errors", 1);
-header('Content-Type: text/html; charset=iso-8859-1');
-
-
-
-echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
-
-$servername = "54.234.153.24";
-$username = "root";
-$password = "Senha123";
-$database = "meubanco";
-
-// Criar conexão
-
-
-$link = new mysqli($servername, $username, $password, $database);
-
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-
-$valor_rand1 =  rand(1, 999);
-$valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
-$host_name = gethostname();
-
-
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
-
-
-if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $link->error;
-}
-
-?>
+  <?php
+  ini_set("display_errors", 1);
+  header('Content-Type: text/html; charset=iso-8859-1');
+  
+  
+  
+  echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
+  
+  $servername = "34.201.150.60";
+  $username = "root";
+  $password = "Senha123";
+  $dbname = "meubanco";
+  
+  // Criar conexão
+  
+  
+  $link = new mysqli($servername, $username, $password, $dbname);
+  
+  /* check connection */
+  if (mysqli_connect_errno()) {
+      printf("Connect failed: %s\n", mysqli_connect_error());
+      exit();
+  }
+  
+  $valor_rand1 =  rand(1, 999);
+  $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
+  $host_name = gethostname();
+  
+  
+  $query = "INSERT INTO dados (
+              AlunoID,
+              Nome,
+              Sobrenome,
+              Endereco,
+              Cidade,
+              Host
+            ) VALUES (
+                '$valor_rand1',
+                '$valor_rand2',
+                '$valor_rand2',
+                '$valor_rand2',
+                '$valor_rand2',
+                '$host_name'
+              )";
+  
+  
+  if ($link->query($query) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $link->error;
+  }
+  
+  ?>
 </body>
 </html>
