4dea51c133f535b98cbb1747bf54b76a7d1fc51d
Hamilton-hbtechcompany <htadeu@hotmail.com>
2022-08-27 22:44:50 -0300

    Conclusão do desafio TOSHIRO SHIBAKITA

--- banco.sql
+++ banco.sql
@@ -2,7 +2,7 @@ CREATE TABLE dados (
 	AlunoID int,
     Nome varchar(50),
     Sobrenome varchar(50),
-    Endereço varchar(150),
+    Endereco varchar(150),
     Cidade varchar(50),
     Host varchar(50)
 );
